2dded03ee515c34fb393a28bf9c22803cbe2b8e5
hrkd <hrkd1316@gmail.com>
2020-01-29 16:15:19 +0900

    Fix bug

--- simu_monty_hall.py
+++ simu_monty_hall.py
@@ -10,19 +10,22 @@ NUM_SIMULATION = int(sys.argv[2])
 for i in range(NUM_SIMULATION):
     door_new_car = random.randrange(NUM_DOORS)
     door_player_choice = random.randrange(NUM_DOORS)
-    door_player_unchoice = [j for j in range(NUM_DOORS) if j != door_player_choice]
+    doors_player_unchoice = [d for d in range(NUM_DOORS) if d != door_player_choice]
 
     if door_player_choice == door_new_car:
-        door_monty_choice = random.choices(door_player_unchoice, k=NUM_DOORS-2)[0]
+        doors_monty_choice = random.sample(doors_player_unchoice, k=NUM_DOORS - 2)
     else:
-        door_player_unchoice_without_car = [j for j in door_player_unchoice if j != door_new_car] 
-        door_monty_choice = random.choices(door_player_unchoice_without_car, k=NUM_DOORS-2)[0]
+        doors_player_unchoice_without_car = [d for d in doors_player_unchoice if d != door_new_car] 
+        doors_monty_choice = random.sample(doors_player_unchoice_without_car, k=NUM_DOORS - 2)
 
-    door_another = [j for j in door_player_unchoice if j != door_monty_choice][0]
+    doors_monty_choice.sort()
+    door_another = [d for d in doors_player_unchoice if not d in doors_monty_choice][0]
 
     if door_player_choice == door_new_car:
+        # print('player_change_win:', i)
         num_unchanged_wins += 1
     elif door_another == door_new_car:
+        # print('player_unchange_win:', i)
         num_changed_wins += 1
 
     prob_changes_wins.append(num_changed_wins / (i + 1))
@@ -36,4 +39,4 @@ plt.plot(prob_changes_wins, label='prob_changes_wins')
 plt.plot(prob_unchanged_wins, label='prob_unchanged_wins')
 plt.legend()
 plt.show()
-plt.savefig('simu_monty_hall.png')
+plt.savefig('simu_monty_hall_' + str(NUM_DOORS) + '.png')
